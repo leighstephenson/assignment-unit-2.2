2ec56abeef3b51e28b115fad595f114bad292e15
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-01-11 21:11:27 -0600

    answered questions 1-6

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -25,10 +25,10 @@ if (number >= 2) {
 */
 
 
-// 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// 1. WRITE YOUR DESCRIPTION HERE:
+// Declaring the variable is "name" and the name is set equal to "Dane".
+// There is a conditional statement where if the name is equal to Mary, the console log will read "Hi, Mary!"
+// If the name is not Mary the console will read "How do you do?"
 //
 
 //CODE
@@ -44,10 +44,10 @@ if (name === 'Mary') {
 
 */
 
-// 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// 2. WRITE YOUR DESCRIPTION HERE:
+// There are two variables, one named secret and one named code. Code is set equal to 123.
+// If the code is equal to 123, the secret is equal to super. 
+// If code is greater than 250 then the code is equal to duper.
 //
 
 //CODE
@@ -69,11 +69,11 @@ console.log(secret)
 
 */
 
-// 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// 3. WRITE YOUR DESCRIPTION HERE:
+// Code declares "isStudent" = true, "age" = 34, and zip= "55407"
+// If "isStudent" equals true and the zip is greater than 80000, the console log will read "You're a student on the West Coast!"
+// Or else if "isStudent" = false and age is less than 30, console log will read "What are your hobbies?". 
+// Or else if "isStudent"= true, console log will read "welcome to Prime", or else the console log will just read "how about the weather?"
 
 //CODE
 /*
@@ -129,6 +129,10 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
+// Fix: The two color values are mixed up, color one is set to red when it should be set to blue. 
+// You would also have to add "colorTwo = purple" to the second state statement, I think! Or it may not be needed
+// Since the two colors mixed to one.
+
 /*
 let colorOne = 'red';
 let colorTwo = 'blue';
@@ -143,6 +147,7 @@ if (mix === true) {
 // We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
+//Fix: Need to add the "&&" to the console.log line.
 /*
 let temp = 40;
 const time = 4;
@@ -156,7 +161,10 @@ if (temp > 39 || time >= 4) {
 // We check if age is greater than or equal to minAge 
 // -- it is, so we console.log 'enter'.
 
+// Fix: We needed to change the sign from less than to greater than, and then need to change the console.log statements to 
+//match the statement where if age is greater than or equal to minAge, then console.log reads enter.
 /*
+
 let age = 21;
 const minAge = 21;
 
